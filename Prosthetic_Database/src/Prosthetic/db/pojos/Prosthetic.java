2cfb3589c0be86fe7a26b34c6ee66a551b574266
GonzaloFernandez45 <gonzalo.fernandezeizaguirre@usp.ceu.es>
2024-04-15 18:14:57 +0200

    working on prosthetic, need and option classes created

--- Prosthetic_Database/src/Prosthetic/db/pojos/Prosthetic.java
+++ Prosthetic_Database/src/Prosthetic/db/pojos/Prosthetic.java
@@ -1,5 +1,90 @@
 package Prosthetic.db.pojos;
 
-public class Prosthetic {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Prosthetic implements Serializable {
+	
+	private int ID;
+	private int Price;
+	private String Size;
+	private List<Need> needs;
+	private List<Option> options;
+	
+	public Prosthetic() {
+		super();
+		this.needs = new ArrayList<Need>();
+		this.options = new ArrayList<Option>();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public int getPrice() {
+		return Price;
+	}
+
+	public void setPrice(int price) {
+		Price = price;
+	}
+
+	public String getSize() {
+		return Size;
+	}
+
+	public void setSize(String size) {
+		Size = size;
+	}
+
+	public List<Need> getNeeds() {
+		return needs;
+	}
+
+	public void setNeeds(List<Need> needs) {
+		this.needs = needs;
+	}
+
+	public List<Option> getOptions() {
+		return options;
+	}
+
+	public void setOptions(List<Option> options) {
+		this.options = options;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prosthetic other = (Prosthetic) obj;
+		return ID == other.ID;
+	}
+
+	@Override
+	public String toString() {
+		return "Prosthetic [ID=" + ID + ", Price=" + Price + ", Size=" + Size + ", needs=" + needs + ", options="
+				+ options + "]";
+	}
+	
+	
+	
+	
 
 }
