55aab83d35baf630115a4508d6da9ad02224b43f
Martadiezr <marta.diezrosa@usp.ceu.es>
2024-04-21 16:25:06 +0200

    start working on interfaces

---
+++ Prosthetic_Database/src/Prosthetic/db/interfaces/CompanyManager.java
@@ -0,0 +1,7 @@
+package Prosthetic.db.interfaces;
+
+import Prosthetic.db.pojos.*;
+
+public interface CompanyManager {
+	public void addCompany(Company a);
+}
---
+++ Prosthetic_Database/src/Prosthetic/db/jdbc/JDBCCompanyManager.java
@@ -0,0 +1,14 @@
+package Prosthetic.db.jdbc;
+
+import Prosthetic.db.interfaces.CompanyManager;
+import Prosthetic.db.pojos.Company;
+
+public class JDBCCompanyManager implements CompanyManager {
+
+	@Override
+	public void addCompany(Company a) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
