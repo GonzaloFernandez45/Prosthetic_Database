ab73d64acfb65cb676be70221628f32de8892237
Martadiezr <marta.diezrosa@usp.ceu.es>
2024-04-29 18:25:38 +0200

    Continue with company and option JDBC

--- Prosthetic_Database/src/Prosthetic/db/interfaces/PatientManager.java
+++ Prosthetic_Database/src/Prosthetic/db/interfaces/PatientManager.java
@@ -10,7 +10,7 @@ public interface PatientManager {
 	public Patient getPatientByID(int id);
 	public List<Patient> getPatientByName(String name);
 	public String reportDelivery(int id);
-	public void inputNeeds(List<Need> needs);
+	public void inputNeeds(Need need);
 
 
 }
