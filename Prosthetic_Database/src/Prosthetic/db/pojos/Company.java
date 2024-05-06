57c106b05b0860def75183b705b512dc09c9d2d2
Martadiezr <marta.diezrosa@usp.ceu.es>
2024-05-06 17:40:05 +0200

    Insert needs by hand after creating the tables.

--- Prosthetic_Database/src/Prosthetic/db/pojos/Company.java
+++ Prosthetic_Database/src/Prosthetic/db/pojos/Company.java
@@ -11,8 +11,6 @@ public class Company implements Serializable {
 	private Integer id;
 	private String name;
 	private String location;
-	
-	//TODO should we consider here to put this or we only put the attribute in the many part?
 	private List <Prosthetic> prosthetics;
 	
 	
